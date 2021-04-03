e51fa187033cab7b162846f24d38bd067467636a
U-ryuryu <kugyuryuryu@gmail.com>
2021-04-03 13:14:09 +0900

    エラー文の設定、バリデーションの追加

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,8 +1,10 @@
 class User < ApplicationRecord
-  # Include default devise modules. Others available are:
-  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+  PASSWORD_REGEX = /\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]+\z/i.freeze
+  NAME_REGEX = /\A[ぁ-んァ-ヶ一-龥々ー]+\z/
 
   validates :nickname,        presence: true
   validates :last_name,       presence: true
@@ -10,4 +12,9 @@ class User < ApplicationRecord
   validates :read_last_name,  presence: true
   validates :read_first_name, presence: true
   validates :birthday,        presence: true
+  validates :password,        format: { with: PASSWORD_REGEX, message: "Use both English and numbers" }, if: Proc.new {|user|user.password.present?}
+  validates :read_last_name,  format: { with: /\A[ァ-ヶー]+\z/, message: "Please enter in the full-width katakana" }, if: Proc.new {|user|user.read_last_name.present?}
+  validates :read_first_name, format: { with: /\A[ァ-ヶー]+\z/, message: "Please enter in the full-width katakana" }, if: Proc.new {|user|user.read_first_name.present?}
+  validates :last_name,       format: { with: NAME_REGEX, message: "Please enter full-width Japanese" }, if: Proc.new {|user|user.last_name.present?}
+  validates :first_name,       format: { with: NAME_REGEX, message: "Please enter full-width Japanese" }, if: Proc.new {|user|user.first_name.present?}
 end
