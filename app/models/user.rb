2dbe37f9d240857fc6469f8dacaa2baa3719a2c5
U-ryuryu <kugyuryuryu@gmail.com>
2021-04-03 18:57:40 +0900

    userモデル修正(with_optionsの使用,正規表現の修正),コメント削除

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,17 +4,20 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
 
   PASSWORD_REGEX = /\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]+\z/i.freeze
-  NAME_REGEX = /\A[ぁ-んァ-ヶ一-龥々ー]+\z/
+  NAME_REGEX = /\A[ぁ-んァ-ヶ一-龥々ー]+\z/.freeze
+  KANA_REGEX = /\A[ァ-ヶー]+\z/.freeze
 
-  validates :nickname,        presence: true
-  validates :last_name,       presence: true
-  validates :first_name,      presence: true
-  validates :read_last_name,  presence: true
-  validates :read_first_name, presence: true
-  validates :birthday,        presence: true
+  with_options presence: true do
+    validates :nickname
+    validates :last_name
+    validates :first_name
+    validates :read_last_name
+    validates :read_first_name
+    validates :birthday
+  end
   validates :password,        format: { with: PASSWORD_REGEX, message: "Use both English and numbers" }, if: Proc.new {|user|user.password.present?}
-  validates :read_last_name,  format: { with: /\A[ァ-ヶー]+\z/, message: "Please enter in the full-width katakana" }, if: Proc.new {|user|user.read_last_name.present?}
-  validates :read_first_name, format: { with: /\A[ァ-ヶー]+\z/, message: "Please enter in the full-width katakana" }, if: Proc.new {|user|user.read_first_name.present?}
+  validates :read_last_name,  format: { with: KANA_REGEX, message: "Please enter in the full-width katakana" }, if: Proc.new {|user|user.read_last_name.present?}
+  validates :read_first_name, format: { with: KANA_REGEX, message: "Please enter in the full-width katakana" }, if: Proc.new {|user|user.read_first_name.present?}
   validates :last_name,       format: { with: NAME_REGEX, message: "Please enter full-width Japanese" }, if: Proc.new {|user|user.last_name.present?}
   validates :first_name,       format: { with: NAME_REGEX, message: "Please enter full-width Japanese" }, if: Proc.new {|user|user.first_name.present?}
 end
