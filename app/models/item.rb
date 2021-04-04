b8b75dc31c2cafff9c9206076a732ad998b7f871
U-ryuryu <kugyuryuryu@gmail.com>
2021-04-04 13:37:40 +0900

    ActiveStorage導入,バリデーション,アソシエーション設定

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,16 +1,18 @@
 class Item < ApplicationRecord
   extend ActiveHash::Associations::ActiveRecordExtensions
-  belongs_to :category
-  belongs_to :status
-  belongs_to :payment
-  belongs_to :prefecture
-  belongs_to :delivery_day
-  belongs_to :user
+  belongs_to       :category
+  belongs_to       :status
+  belongs_to       :payment
+  belongs_to       :prefecture
+  belongs_to       :delivery_day
+  belongs_to       :user
+  has_one_attached :image
 
   with_options presence: true do
     validates :name 
     validates :description
     validates :price
+    validates :image
   end
 
   with_options numericality: { other_than: 1 } do
