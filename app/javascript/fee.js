00c113dc717fa276b7503febba1497669bda0b4b
U-ryuryu <kugyuryuryu@gmail.com>
2021-04-05 00:40:08 +0900

    入力された販売価格に応じて販売手数料や販売利益の表示を変更する機能の導入

---
+++ app/javascript/fee.js
@@ -0,0 +1,22 @@
+function calc (){
+  const price = document.getElementById("item-price");
+  const feePrice = document.getElementById("add-tax-price");
+  const profitPrice = document.getElementById("profit");
+  if (price.value != "") {
+    feePrice.innerHTML = Math.round(price.value / 10);
+    profitPrice.innerHTML = price.value - Math.round(price.value / 10);
+  }else{
+    feePrice.innerHTML = 0;
+    profitPrice.innerHTML = 0;
+  }
+
+  price.addEventListener("keyup", () => {
+    const originalPrice = price.value;
+    const fee = Math.round(originalPrice / 10);
+    const profit = originalPrice - fee;
+    feePrice.innerHTML = fee;
+    profitPrice.innerHTML = profit;
+  })
+}
+
+window.addEventListener('load', calc)
