f26f4396e06a864c2ca95e76439ffc9bdf4c9724
U-ryuryu <kugyuryuryu@gmail.com>
2021-04-07 00:00:53 +0900

    ログイン状態の出品者以外が購入画面に遷移できる機能の追加

---
+++ app/controllers/purchases_controller.rb
@@ -0,0 +1,19 @@
+class PurchasesController < ApplicationController
+  before_action :authenticate_user!
+  before_action :item_find
+  before_action :redirect_seller
+  
+
+  def index
+  end
+
+  private
+
+  def item_find
+    @item = Item.find(params[:item_id])
+  end
+
+  def redirect_seller
+    redirect_to root_path if current_user.id == @item.user.id
+  end
+end
