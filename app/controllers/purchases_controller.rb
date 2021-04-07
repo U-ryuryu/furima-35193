ee70a7dc0bc4cd177a350f42ee5e45fd8cc9d6cd
U-ryuryu <kugyuryuryu@gmail.com>
2021-04-07 19:22:24 +0900

    Payjpによる購入機能の導入、購入確認完了、APIキーは環境変数に格納済(Formオブジェクト実装時に行う処理はコメントアウトにてスキップ)

--- app/controllers/purchases_controller.rb
+++ app/controllers/purchases_controller.rb
@@ -5,9 +5,18 @@ class PurchasesController < ApplicationController
   
 
   def index
+    @purchase = Purchase.new
   end
 
   def create
+    # @purchase = Purchase.new(purchase_params)  formオブジェクト追加後実装
+    # if @purchase.valid?   formオブジェクト追加後実装
+      pay_item
+      # @purchase.save    formオブジェクト追加後実装 return
+      redirect_to root_path
+    # else
+    #   render 'index'   formオブジェクト追加後実装
+    # end
   end
 
   private
@@ -18,5 +27,18 @@ class PurchasesController < ApplicationController
 
   def redirect_seller
     redirect_to root_path if current_user.id == @item.user.id
+  end
+
+  def purchase_params
+    params.permit(:token, :item_id).merge(user_id: current_user.id)
+  end
+
+  def pay_item
+    Payjp.api_key = ENV["PAYJP_SECRET_KEY"]
+    Payjp::Charge.create(
+      amount: @item.price,
+      card: params[:token],
+      currency: 'jpy'
+    )
   end
 end
